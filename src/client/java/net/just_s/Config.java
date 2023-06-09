f2e1652f4baaa67b34c13c10fbf39eb2d4abce55
Just <grivten@yandex.ru>
2023-06-09 13:39:18 +0300

    v1

---
+++ src/client/java/net/just_s/Config.java
@@ -0,0 +1,22 @@
+package net.just_s;
+
+public class Config {
+    public static boolean START_FROM_ZERO = false;
+    public static boolean SHOULD_USE_CUSTOM_TEXT = false;
+    public static String CUSTOM_TEXT = "Custom Text %d";
+
+    public static boolean BOLD = true;
+    public static boolean ITALIC = false;
+    public static boolean OBFUSCATED = false;
+    public static boolean STRIKETHROUGH = false;
+    public static boolean UNDERLINE = false;
+
+    public static float HUD_X = 10f;
+    public static float HUD_Y = 10f;
+    public static String HUD_COLOR = "#FFFFFF";
+
+    public static boolean SHOULD_DRAW_SHADOW = true;
+    public static float SHADOW_RELATIVE_X = 1f;
+    public static float SHADOW_RELATIVE_Y = 1f;
+    public static String SHADOW_COLOR = "#000000";
+}
---
+++ src/client/java/net/just_s/WorldDaysModClient.java
@@ -0,0 +1,64 @@
+package net.just_s;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
+import net.minecraft.text.TextColor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WorldDaysModClient implements ClientModInitializer {
+    public static final Logger LOGGER = LoggerFactory.getLogger("world-days");
+    public static final MinecraftClient MC = MinecraftClient.getInstance();
+
+    @Override
+    public void onInitializeClient() {
+        HudRenderCallback.EVENT.register(this::renderText);
+    }
+
+    private void renderText(MatrixStack matrixStack, float v) {
+        if (MC.options.debugEnabled) return;
+
+        // Calculate current day int he world
+        long day = MC.world.getTimeOfDay() / 24000L;
+        if (!Config.START_FROM_ZERO) {
+            day += 1;
+        }
+
+        // Generate style
+        Style style = Style.EMPTY
+                .withBold(Config.BOLD)
+                .withItalic(Config.ITALIC)
+                .withObfuscated(Config.OBFUSCATED)
+                .withStrikethrough(Config.STRIKETHROUGH)
+                .withUnderline(Config.UNDERLINE);
+
+        // Figure out what to render
+        Text text;
+        if (Config.SHOULD_USE_CUSTOM_TEXT) {
+            text = Text.translatable(Config.CUSTOM_TEXT, day).setStyle(style);
+        } else {
+            text = Text.translatable("stat.world-days.day", day).setStyle(style);
+        }
+
+        // Render Shadow first if needed
+        if (Config.SHOULD_DRAW_SHADOW) {
+            MC.textRenderer.draw(
+                    matrixStack, text,
+                    Config.HUD_X + Config.SHADOW_RELATIVE_X,
+                    Config.HUD_Y + Config.SHADOW_RELATIVE_Y,
+                    TextColor.parse(Config.SHADOW_COLOR).getRgb()
+            );
+        }
+
+        // Render text
+        MC.textRenderer.draw(
+                matrixStack, text,
+                Config.HUD_X, Config.HUD_Y,
+                TextColor.parse(Config.HUD_COLOR).getRgb()
+        );
+    }
+}
